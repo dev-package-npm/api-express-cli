c154e21ae3abe8beecc6127bead422e6f02b2499
Jairo Rohatan <jarohatan@gmail.com>
2023-03-04 20:54:11 -0500

    se añade nuevo archivo para manejar compilación de paquete y compilación de prueba para servidor.

--- lib/bin/aec.ts
+++ lib/bin/aec.ts
@@ -3,8 +3,8 @@ import ansiColors from 'ansi-colors';
 import { exec } from 'child_process';
 import fs from 'fs';
 import path from 'path';
-import cliProgress from 'cli-progress';
-import Spinner from 'ts-spinner';
+import Spinnies from 'spinnies';
+
 import { controller } from '../cli/controller.cli';
 import { entity } from '../cli/entity.cli';
 import { startStructure } from '../cli';
@@ -79,7 +79,7 @@ COMMAND LINE FLAGS
             else if (fs.existsSync(this.pathPackage)) {
                 if (params == 'init' || params == 'in') {
                     await this.startStructure();
-                    // interpretAttibutes(input);
+                    this.interpretAttibutes(this.input);
                 }
                 else if (params == 'entity' || params == 'e') {
                     // if (fs.existsSync(path.resolve() + '/' + config1.dir + '/models'))
@@ -87,7 +87,7 @@ COMMAND LINE FLAGS
                     // else console.log(ansiColors.yellowBright('You can\'t create an entity because you haven\'t added the database module. '), ansiColors.blueBright('Use aec add db:mysql'));
                 }
                 else if (params == 'route' || params == 'r') {
-                    // await route();
+                    await route();
                 }
                 else if (params == 'controller' || params == 'c') {
                     // await controller();
@@ -134,170 +134,88 @@ COMMAND LINE FLAGS
     }
 
     private async startStructure() {
-        return new Promise((resolve, reject) => {
-            const pathWork = path.resolve() + '/' + this.structureProject.dir;
-            // console.log(pathWork);
+        const promise = new Promise(async (resolve, rejects) => {
+            const spinnies = new Spinnies();
+            try {
+                const pathWork = path.resolve() + '/' + this.structureProject.dir;
+                // ! quitar
+                // if (fs.existsSync(pathWork))
+                //     fs.rmSync(pathWork, { recursive: true });
+                if (!fs.existsSync(pathWork)) {
+                    const devPackages = '@types/morgan @types/express @types/node @types/bcryptjs @types/cryptr @types/jsonwebtoken nodemon typescript';
+                    const _package = 'express morgan dotenv dotenv-expand bcryptjs cryptr jsonwebtoken';
+                    spinnies.add('spinner-1', { text: ansiColors.blueBright('Installing packages...') });
 
-            // ! quitar
-            if (fs.existsSync(pathWork))
-                fs.rmSync(pathWork, { recursive: true });
-            if (!fs.existsSync(pathWork)) {
-                const devPackages = '@types/morgan @types/express @types/node @types/bcryptjs @types/cryptr @types/jsonwebtoken nodemon typescript';
-                const _package = 'express morgan dotenv dotenv-expand bcryptjs cryptr jsonwebtoken';
-                // const barInstalling = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-                // barInstalling.start(200, 0);
-                // const spinner = new Spinner();
-                // spinner.start(ansiColors.blueBright('Installing packages...'));
-
-                // const load = loading({ text: 'Installing packages...', color: 'blue' });
-                // load.start();
-
-                exec('npm i ' + _package, (error, stdout, stderr) => {
-                    if (!error && stdout != '' && !stderr) {
-                        const ex = exec('npm i -D ' + devPackages, async (error, stdout, stderr) => {
-                            // spinner.success(ansiColors.blueBright('Done installation'));
-                            // spinner.stop();
-                            // barInstalling.stop();
-                            // load.stop();
-                            console.log(ansiColors.blueBright('✓ Done installation'));
-                            if (!error && stdout != '' && !stderr) {
-                                const folders = this.getSubdirs().filter(value => value != 'models' && value != 'databases' && value != 'files');
-                                folders.forEach((value) => {
-                                    fs.mkdirSync(pathWork + '/' + value, { recursive: true });
-                                });
-                                Object.keys(this.structureProject.subDir.testing).forEach((value) => {
-                                    fs.mkdirSync(pathWork + '/testing/' + value, { recursive: true });
-                                });
-                                if (fs.existsSync(pathWork + '/routes/'))
-                                    fs.writeFileSync(pathWork + '/routes/' + this.routeIndex(0).fileName, this.routeIndex(0).write());
-                                if (fs.existsSync(pathWork + '/testing/routes/')) {
-                                    fs.writeFileSync(pathWork + '/testing/routes/' + this.routeIndex(1).fileName, this.routeIndex(1).write());
-                                }
-                                this.createEnvFile();
-                                // Create core files
-                                this.createControllerCore();
-                                this.createSecurityCore();
-                                // Create Server http
-                                this.createMiddlewares();
-                                await this.createServerHttp();
-                                this.createIndexApi();
-                                if (!fs.existsSync(path.resolve() + '/tsconfig.json')) {
-                                    exec(`npx tsc --init --target ES2022 --removeComments true --outDir ./${this.folderBuild}`, (error, stdout, tderr) => {
-                                        if (!error && stdout != '' && !tderr) {
-                                            this.addLineFilePackage(this.folderBuild);
-                                            resolve(true);
-                                        }
-                                        else {
-                                            console.log(error || stderr);
-                                            reject(false);
-                                        }
-                                    });
-
-                                } else resolve(true);
-                            } else {
-                                console.log(error || stderr);
-                                reject(false);
-                            }
-                        });
-                        // ex.on('spawn', () => {
-                        //     console.log('spawn', ex.stdout);
-                        // })
-                    } else {
-                        console.log(error || stderr);
-                        reject(false);
+                    await this.executeTerminal(`npm i ${_package}`);
+                    await this.executeTerminal(`npm i ${devPackages} -D`);
+                    spinnies.succeed('spinner-1', { text: ansiColors.blueBright('Done installation') });
+                    const folders = this.getSubdirs().filter(value => value != 'models' && value != 'databases' && value != 'files');
+                    folders.forEach((value) => {
+                        fs.mkdirSync(pathWork + '/' + value, { recursive: true });
+                    });
+                    Object.keys(this.structureProject.subDir.testing).forEach((value) => {
+                        fs.mkdirSync(pathWork + '/testing/' + value, { recursive: true });
+                    });
+                    if (fs.existsSync(pathWork + '/routes/'))
+                        fs.writeFileSync(pathWork + '/routes/' + this.routeIndex(0).fileName, this.routeIndex(0).write());
+                    if (fs.existsSync(pathWork + '/testing/routes/')) {
+                        fs.writeFileSync(pathWork + '/testing/routes/' + this.routeIndex(1).fileName, this.routeIndex(1).write());
                     }
-                });
-
-            }
-            else {
-                console.error(ansiColors.redBright('A project has already been initialized'));
-                reject(false);
+                    this.createEnvFile();
+                    // Create core files
+                    this.createControllerCore();
+                    this.createSecurityCore();
+                    // Create Server http
+                    this.createMiddlewares();
+                    await this.createServerHttp();
+                    this.createIndexApi();
+                    if (!fs.existsSync(path.resolve() + '/tsconfig.json')) {
+                        await this.executeTerminal(`npx tsc --init --target ES2022 --removeComments true --outDir ./${this.folderBuild}`);
+                        await this.addLineFilePackage(this.folderBuild);
+                    }
+                    resolve(true);
+                }
+                else {
+                    rejects(new Error('A project has already been initialized'));
+                }
+            } catch (error: any) {
+                spinnies.fail('spinner-1', { text: ansiColors.blueBright(error.message) });
+                rejects(new Error(error.message));
             }
         });
+
+        return await promise;
     };
+
+    private executeTerminal(params: string): Promise<string> {
+        return new Promise((resovle, rejects) => {
+            exec(params, (error, stdout, stderr) => {
+                if (error != null)
+                    rejects(new Error(String(error)));
+                if (stderr != '')
+                    rejects(new Error(String(stderr)));
+                resovle(stdout);
+            });
+        });
+    }
+
+    private interpretAttibutes(input: Array<string>) {
+        if (input.length > 2) {
+            var attributes = input.slice(2);
+            switch (attributes[0]) {
+                case '--add':
+                    attributes = attributes.slice(1);
+                    attributes.forEach(async element => {
+                        await addUtilities(element);
+                    });
+                    break;
+                case '--name':
+                    break;
+                default:
+                    throw new Error('Invalid attribute');
+                    break;
+            }
+        } else throw new Error('A value is expected in the argument');
+    }
     //#endregion
 }
-
-//         process.title = "aec " + Array.from(process.argv).slice(2).join(" ");
-//         const input = process.title.split(" ");
-//         const params = input[1];
-
-//         if (params === "--help" || params === "-h" || params == "") {
-//             const help = `
-// Example command
-//     ${ansiColors.cyan('aec <command> --help ')}More information
-//     ${ansiColors.cyan('aec <flags> <options> ')}More information
-// COMMAND LINE FLAGS
-
-//     ${ansiColors.cyan('init, in ')}Initialize a folder structure for the api, with some utilities.
-//     ${ansiColors.cyan('entity, e ')}Create a set of files: route, controller and model.
-//     ${ansiColors.cyan('route, r ')}Create a route with the specified name.
-//     ${ansiColors.cyan('controller, c ')}Create a controller with the specified name.
-//     ${ansiColors.cyan('model, m ')}Create a model with the specified name.
-//     ${ansiColors.cyan('add, ad ')}Allow adding new features.
-//     ${ansiColors.cyan('remove, rm ')}Removes a package, which is added with the --add command.
-//     ${ansiColors.cyan('--help, -h ')}Print this message.
-//     ${ansiColors.cyan('--version, -v ')}Print version with package.
-
-// COMMAND OPTIONS
-//     ${ansiColors.cyan('--name ')}Name files ${ansiColors.redBright('Not applied')}.
-//     ${ansiColors.cyan('--add ')}Name module.
-//     `;
-//             console.log(help);
-//         }
-//         else if (params == 'init' || params == 'in') {
-//             await startStructure();
-//             interpretAttibutes(input);
-//         }
-//         else if (params == 'entity' || params == 'e') {
-//             if (fs.existsSync(path.resolve() + '/' + config1.dir + '/models'))
-//                 await entity();
-//             else console.log(ansiColors.yellowBright('You can\'t create an entity because you haven\'t added the database module. '), ansiColors.blueBright('Use aec add db:mysql'));
-//         }
-//         else if (params == 'route' || params == 'r') {
-//             await route();
-//         }
-//         else if (params == 'controller' || params == 'c') {
-//             await controller();
-//         }
-//         else if (params == 'model' || params == 'm') {
-//             if (fs.existsSync(path.resolve() + '/' + config1.dir + '/models'))
-//                 await model();
-//             else console.log(ansiColors.yellowBright('You can\'t create an entity because you haven\'t added the database module. '), ansiColors.blueBright('Use aec add db:mysql'));
-//         }
-//         else if (params == 'add' || params == 'ad') {
-//             await addUtilities(input.slice(2)[0]);
-//         }
-//         else if (params == 'remove' || params == 'rm') {
-//             await removeModules(input.slice(2));
-//         }
-//         else if (params == '-v' || params == '--version') {
-//             // console.log('Version', ansiColors.cyan(version));
-//         }
-//         else console.log(ansiColors.yellowBright('Command is not valid'));
-//     } catch (error: any) {
-//         console.error(ansiColors.redBright(error.message));
-//     }
-// };
-
-// const interpretAttibutes = (input: Array<string>) => {
-//     if (input.length > 2) {
-//         var attributes;
-//         attributes = input.slice(2);
-//         switch (attributes[0]) {
-//             case '--add':
-//                 attributes = attributes.slice(1);
-//                 attributes.forEach(async element => {
-//                     await addUtilities(element);
-//                 });
-//                 break;
-//             case '--name':
-//                 break;
-//             default:
-//                 console.log(ansiColors.redBright('Invalid attribute'));
-//                 break;
-//         }
-//     }
-// }
-
-// main();
